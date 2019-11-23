77d01c8a42f0567463c8744d215a526bde2f01d3
aendrevv <andrew.tolmachov@gmail.com>
2019-11-23 21:59:03 +0200

    .prettierrc.json was added

--- Meeting-2-project-setup-homework/src/index.js
+++ Meeting-2-project-setup-homework/src/index.js
@@ -1,13 +1,13 @@
-const http = require("http"),
-  nanoid = require("nanoid"),
-  hostname = "127.0.0.1",
+const http = require('http'),
+  nanoid = require('nanoid'),
+  hostname = '127.0.0.1',
   port = 3000,
   server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(nanoid());
-  });
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(nanoid())
+  })
 
 server.listen(port, hostname, () =>
   console.log(`Server is running at http://${hostname}:${port}/`)
-);
+)
