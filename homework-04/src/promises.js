cdc26db64f06cb8f99a7f071c6295a1f01545649
aendrevv <andrew.tolmachov@gmail.com>
2019-12-17 23:17:26 +0200

    promise chaining final

--- homework-04/src/promises.js
+++ homework-04/src/promises.js
@@ -1,15 +1,19 @@
-const { throwDice, getSumAfterOneSec } = require('./myFunctions');
+const { throwDice } = require('./myFunctions');
 
 const start = Date.now();
+let justAnotherSum = 0;
 
 throwDice(700)
   .then(dice1 => {
-    console.log(`Running time: ${Date.now() - start}; First dice is: ${dice1}`);
-    throwDice(1300).then(dice2 => {
-      console.log(`Running time: ${Date.now() - start}; Second dice is: ${dice2}`);
-      getSumAfterOneSec(dice1, dice2).then(sum =>
-        console.log(`Running time: ${Date.now() - start}; The sum is: ${sum}`)
-      );
-    });
+    console.log(`Running time: ${Date.now() - start}; The sum is: ${dice1}`);
+    justAnotherSum += dice1;
+    return throwDice(1300);
+  })
+  .then(dice2 => {
+    console.log(`Running time: ${Date.now() - start}; The sum is: ${dice2}`);
+    justAnotherSum += dice2;
+  })
+  .then(() => {
+    setTimeout(() => console.log(`Running time: ${Date.now() - start}; The sum is: ${justAnotherSum}`), 1000);
   })
   .catch(err => console.log(err));
