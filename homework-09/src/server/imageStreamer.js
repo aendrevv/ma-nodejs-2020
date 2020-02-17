89aecc7ed41d3450e85f2ffd715fd0ab2029154f
aendrevv <andrew.tolmachov@gmail.com>
2020-02-17 22:01:14 +0200

    useless file was deleted; error handling was added; config was fixed.

--- homework-09/src/server/imageStreamer.js
+++ homework-09/src/server/imageStreamer.js
@@ -2,28 +2,45 @@ const fs = require('fs');
 const config = require('../config');
 
 const { Transform, pipeline } = require('stream');
+class transformStream extends Transform {
+  constructor() {
+    super();
+    this.currentSize = 0;
+  }
 
-let chunksCounter = 0;
-const speed = Math.min(1024, Math.max(128, config.speed));
-const timeout = (64 * 1000) / speed;
-
-const reportProgress = new Transform({
-  transform(chunk, encoding, callback) {
-    if (++chunksCounter % 16 === 0) process.stdout.write('. ');
-
+  _transform(chunk, encoding, callback) {
+    const timeout = (1000 * chunk.length) / Math.max(config.speedMinimal, config.speedCustom);
+    this.currentSize += chunk.length;
+    if (this.currentSize >= 1024 * 1024) {
+      process.stdout.write('. ');
+      this.currentSize = 0;
+    }
     setTimeout(() => {
       this.push(chunk);
       callback();
     }, timeout);
-  },
-});
+  }
+}
 
 function streamJPEG(response) {
-  console.log(`\nHere we go on ${speed} kBps!\n`);
+  console.log(`\nStart on ${Math.max(config.speedMinimal, config.speedCustom) / 1024} kBps!\n`);
+
   const readableStream = fs.createReadStream(config.filePath);
-  pipeline(readableStream, reportProgress, response, err => {
-    if (err) console.error('\nDownloading failed:\n', err.stack);
-    console.log('\n\nDownloading succeeded!');
+  const speedController = new transformStream();
+
+  readableStream.on('error', error => {
+    console.error('Error in image reading !', error.stack);
+    response.emit('error', new Error('Error in image reading !'));
+  });
+  speedController.on('error', error => {
+    console.error('Error in image streaming !', error.stack);
+    response.emit('error', new Error('Error in image streaming !'));
+  });
+
+  pipeline(readableStream, speedController, response, err => {
+    if (err) {
+      console.error('\nDownloading failed:\n', err.stack);
+    } else console.log('\n\nDownloading succeeded!');
   });
 }
 
